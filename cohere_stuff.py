fc329e0498ba9e3d2999582fda6b62f3ae9bb91d
Joseph Lu <josephlu414@gmail.com>
2022-09-17 14:16:56 -0400

    Update Cohere File

--- cohere_stuff.py
+++ cohere_stuff.py
@@ -5,6 +5,8 @@ from scraper import getProjectData
 import threading
 from os.path import exists
 
+NUMPROJECTREFERENCES = 10
+
 # what we're trying to do
 TASK_DESCRIPTION = "This program will generate a project title and project subtitle for a hackathon project."
 
@@ -13,83 +15,89 @@ STOP_SEQUENCE = "--"
 DATA_FILE = "data.json"
 
 # retrieves a generated idea from our store
+
+
 def get_idea():
-  if not exists(DATA_FILE):
-    data = []
-    with open(DATA_FILE, "w+") as f:
-      f.write(json.dumps(data))
-
-  with open(DATA_FILE) as f:
-    data = json.load(f)
-  
-  fail_count = 0
-  while len(data) == 0:
-    print("no ideas ready")
-    fail_count += 1
-    if (fail_count >= 3):
-      return {
-        "message": "All out of new ideas :("
-      }
-
-    generate()
+    if not exists(DATA_FILE):
+        data = []
+        with open(DATA_FILE, "w+") as f:
+            f.write(json.dumps(data))
 
     with open(DATA_FILE) as f:
-      data = json.load(f)
-  
-  idea = data.pop(0)
+        data = json.load(f)
 
-  with open(DATA_FILE, "w") as f:
-    f.write(json.dumps(data))
+    fail_count = 0
+    while len(data) == 0:
+        print("no ideas ready")
+        fail_count += 1
+        if (fail_count >= 3):
+            return {
+                "message": "All out of new ideas :("
+            }
 
-  if (len(data) <= 5):
-    print("restocking ideas")
-    thread = threading.Thread(target=generate)
-    thread.start()
+        generate()
 
-  return idea
+        with open(DATA_FILE) as f:
+            data = json.load(f)
+
+    idea = data.pop(0)
+
+    with open(DATA_FILE, "w") as f:
+        f.write(json.dumps(data))
+
+    if (len(data) <= 5):
+        print("restocking ideas")
+        thread = threading.Thread(target=generate)
+        thread.start()
+
+    return idea
 
 # generates new ideas
+
+
 def generate():
-  print("generating new ideas...")
-  with open('key.txt', 'r') as file:
-    KEY = file.read().replace('\n', '')
+    print("generating new ideas...")
+    with open('key.txt', 'r') as file:
+        KEY = file.read().replace('\n', '')
 
-  co = cohere.Client(KEY)
+    co = cohere.Client(KEY)
 
-  data = getProjectData(5, "https://devpost.com/software/search?query=is%3Afeatured")
+    data = getProjectData(
+        NUMPROJECTREFERENCES, "https://devpost.com/software/search?query=is%3Afeatured")
 
-  prompt = TASK_DESCRIPTION + "\n" + "".join(map(lambda project: (
-    STOP_SEQUENCE + "\n" +
-    "Project title: " + project["title"] + "\n" +
-    "Project subtitle: " + project["subtitle"] + "\n"
-    # "Project description: " + project["description"] + "\n"
-  ), data)) + STOP_SEQUENCE + "\n"
+    prompt = TASK_DESCRIPTION + "\n" + "".join(map(lambda project: (
+        STOP_SEQUENCE + "\n" +
+        "Project title: " + project["title"] + "\n" +
+        "Project subtitle: " + project["subtitle"] + "\n"
+        # "Project description: " + project["description"] + "\n"
+    ), data)) + STOP_SEQUENCE + "\n"
 
-  print("prompt:", prompt)
+    print("prompt:", prompt)
 
-  response = co.generate(
-    prompt= prompt,
-    max_tokens=300
-  )
+    response = co.generate(
+        prompt=prompt,
+        max_tokens=300,
+        temperature=1.0
+    )
 
-  # not 100% sure about the output from the api, may try to produce more than one post
-  # so we take the first one
-  ideas = response.generations[0].text.strip().split("\n--\n")
+    # not 100% sure about the output from the api, may try to produce more than one post
+    # so we take the first one
+    ideas = response.generations[0].text.strip().split("\n--\n")
 
-  ideas_processed = []
-  for idea in ideas:
-    print("idea", idea)
-    re_results = re.search("Project title: (.*)\nProject subtitle: (.*)", idea.strip())
-    if not re_results:
-      break
-    ideas_processed.append({
-      "title": re_results.group(1),
-      "subtitle": re_results.group(2),
-    })
+    ideas_processed = []
+    for idea in ideas:
+        print("idea", idea)
+        re_results = re.search(
+            "Project title: (.*)\nProject subtitle: (.*)", idea.strip())
+        if not re_results:
+            break
+        ideas_processed.append({
+            "title": re_results.group(1),
+            "subtitle": re_results.group(2),
+        })
 
-  
-  with open(DATA_FILE, "r") as f:
-    data = json.load(f)
-    data = data + ideas_processed
-  with open(DATA_FILE, "w") as f:
-    f.write(json.dumps(data))
+    with open(DATA_FILE, "r") as f:
+        data = json.load(f)
+        data = data + ideas_processed
+    with open(DATA_FILE, "w") as f:
+        f.write(json.dumps(data))
