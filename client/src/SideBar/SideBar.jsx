0616a256fa6dc0118a79c8bcd886378301a0ba4c
Emily Yu <35238826+emily-emily@users.noreply.github.com>
2022-09-17 19:32:23 -0400

    Charlesakintunde/UI updates (#8)
    idea Page
    
    Co-authored-by: Ireoluwa0011 <99848171+Ireoluwa0011@users.noreply.github.com>

---
+++ client/src/SideBar/SideBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import SideButtons from '../SideButtons/SideButtons'
+import './SideBar.css'
+
+
+const SideBar = () => {
+  return (
+    <div className='sidebar'>
+       <div className='type'>
+        <p>Type:</p>
+        <div>
+        <SideButtons text={"Quick Idea"}/>
+        <SideButtons text={"Full DevPost"}/>
+        </div>
+       </div>
+
+       <div>
+       <p> Filter: </p>
+        <SideButtons text={"Blockchain"}/>
+        <SideButtons text={"Hardware"}/>
+        <SideButtons text={"NLP"}/>
+        <SideButtons text={"IOT"}/>
+        <SideButtons text={"AI / ML"}/>
+        <SideButtons text={"Computer Vision"}/>
+       </div>
+    </div>
+  )
+}
+
+
+export default SideBar
