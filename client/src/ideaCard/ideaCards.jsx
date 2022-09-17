7c0e7a4dbf0159447334f6cd64febb496c7e4b53
Ireoluwa0011 <99848171+Ireoluwa0011@users.noreply.github.com>
2022-09-17 19:32:27 -0300

    idea Page setUp

---
+++ client/src/ideaCard/ideaCards.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Paper from '@mui/material/Paper';
+
+
+const ideaCards = () => {
+  return (
+    <div className='ideaCard'>
+        Your Topic:
+
+        <Paper/>
+    </div>
+  )
+}
+
+export default ideaCards
